6f2b48123b5d116855869fd3af53cbf178be6f5e
Marcelo <49683518+marcelomagnobarroso@users.noreply.github.com>
2023-05-03 20:13:37 -0300

    [SPRINT1-CREATE_SCHEMAS] Criado pasta e arquivos da representação dos esquemas.

---
+++ schemas/__init__.py
@@ -0,0 +1,5 @@
+from schemas.comentario import ComentarioSchema
+from schemas.produto import ProdutoSchema, ProdutoBuscaSchema, ProdutoViewSchema, \
+                            ListagemProdutosSchema, ProdutoDelSchema, apresenta_produtos, \
+                            apresenta_produto, apresenta_produtos, ProdutoNameBuscaSchema
+from schemas.error import ErrorSchema
